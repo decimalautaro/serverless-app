b85d0f1c43bf2dbf9975a1c3883edfc526eda12f
Lautaro Decima <decimalautaro@gmail.com>
2023-07-28 04:52:03 -0300

    add: get all tasks

---
+++ src/getAllTasks.js
@@ -0,0 +1,23 @@
+const AWS = require('aws-sdk')
+
+const getAllTasks = async (event) => {
+    try {
+        const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+        const result = await dynamodb.scan({
+            TableName: 'TaskTable'
+        }).promise()
+
+        const tasks = result.Items
+        return {
+            status: 200,
+            body: { tasks }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    getAllTasks,
+}
